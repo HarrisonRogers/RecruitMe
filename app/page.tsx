206a4688c3a8dba8b99e584d5aca736d15be6b4e
HarrisonRogers <harrison.rogers05@gmail.com>
2024-05-17 10:03:33 +1200

    inital downlaods

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from '@/components/ui/button'
+import { Camera } from 'lucide-react'
+
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <Button>default button</Button>
+      <Button variant={'outline'} size={'icon'}>
+        <Camera />
+      </Button>
+    </div>
+  )
 }
