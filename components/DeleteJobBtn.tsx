3dab7f926e365bf6d9b9445f345acf42018416fe
HarrisonRogers <harrison.rogers05@gmail.com>
2024-05-22 11:09:32 +1200

    add searhc files

---
+++ components/DeleteJobBtn.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const DeleteJobBtn = () => {
+  return <h1 className="text-4xl">DeleteJobBtn</h1>
+}
+
+export default DeleteJobBtn
---
+++ components/JobsList.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const JobsList = () => {
+  return <h1 className="text-4xl">JobsList</h1>
+}
+
+export default JobsList
