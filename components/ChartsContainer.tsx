50c14b6e31c67e3db92000f2a96de0797a315d88
HarrisonRogers <harrison.rogers05@gmail.com>
2024-05-23 11:52:42 +1200

    chart added

--- components/ChartsContainer.tsx
+++ components/ChartsContainer.tsx
@@ -1,7 +1,49 @@
+'use client'
 import React from 'react'
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts'
+
+import { useQuery } from '@tanstack/react-query'
+import { getChartsDataAction } from '@/utils/actions'
+import { useTheme } from 'next-themes'
 
 const ChartsContainer = () => {
-  return <div className="text-4xl">ChartsContainer</div>
+  const { data, isPending } = useQuery({
+    queryKey: ['charts'],
+    queryFn: () => getChartsDataAction(),
+  })
+
+  const { theme } = useTheme()
+
+  if (isPending) return <h2 className="text-xl font-medium">Please wait...</h2>
+  if (!data || data.length < 1) return null
+  return (
+    <section className="mt-16">
+      <h1 className="text-4xl font-semibold text-center">
+        Monthly Applications
+      </h1>
+      <ResponsiveContainer width={'100%'} height={300}>
+        <BarChart data={data} margin={{ top: 50 }}>
+          <CartesianGrid strokeDasharray={'3 3'} />
+          <XAxis dataKey={'date'} />
+          <YAxis allowDecimals={false} />
+          <Tooltip />
+          <Bar
+            dataKey={'count'}
+            fill={theme === 'dark' ? '#EA580C' : '#F97316'}
+            barSize={75}
+          />
+        </BarChart>
+      </ResponsiveContainer>
+    </section>
+  )
 }
 
 export default ChartsContainer
